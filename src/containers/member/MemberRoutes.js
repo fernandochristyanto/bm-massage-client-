5545717fc4a43389fb15302d0bc0bb02125f07c2
Fernando Christyanto <christyantofernando@gmail.com>
2018-09-10 09:29:21 +0700

    Finish prototype and main func

--- src/containers/member/MemberRoutes.js
+++ src/containers/member/MemberRoutes.js
@@ -5,45 +5,45 @@ import Home from './Home';
 import MassagePlaceDetail from './MassagePlaceDetail';
 
 class MemberRoutes extends Component {
-    constructor(props) {
-        super(props);
+  constructor(props) {
+    super(props);
 
-        this.state = {
-            selectedMassagePlace: undefined
-        }
+    this.state = {
+      selectedMassagePlace: undefined
     }
+  }
 
-    setSelectedMassagePlace = (massagePlace) => {
-        this.setState({ ...this.state, selectedMassagePlace: massagePlace })
-    }
-    render() {
-        const { match } = this.props;
-        return (
-            <div id="router">
-                <Switch>
-                    <Route path={`${match.url}/home`} render={props => {
-                        return (
-                            <Home
-                                {...props}
-                                setSelectedMassagePlace={this.setSelectedMassagePlace}
-                            />
-                        )
-                    }} />
+  setSelectedMassagePlace = (massagePlace) => {
+    this.setState({ ...this.state, selectedMassagePlace: massagePlace })
+  }
+  render() {
+    const { match } = this.props;
+    return (
+      <div id="router">
+        <Switch>
+          <Route path={`${match.url}/home`} render={props => {
+            return (
+              <Home
+                {...props}
+                setSelectedMassagePlace={this.setSelectedMassagePlace}
+              />
+            )
+          }} />
 
-                    {this.state.selectedMassagePlace &&
-                        <Route path={`${match.url}/massageplace/d/:massageplaceid`} render={props => {
-                            return (
-                                <MassagePlaceDetail
-                                    {...props}
-                                    selectedMassagePlace={this.state.selectedMassagePlace}
-                                />
-                            )
-                        }} />
-                    }
-                </Switch>
-            </div>
-        )
-    }
+          {this.state.selectedMassagePlace &&
+            <Route path={`${match.url}/massageplace/d/:massageplaceid`} render={props => {
+              return (
+                <MassagePlaceDetail
+                  {...props}
+                  selectedMassagePlace={this.state.selectedMassagePlace}
+                />
+              )
+            }} />
+          }
+        </Switch>
+      </div>
+    )
+  }
 }
 
 export default withRouter(MemberRoutes);
